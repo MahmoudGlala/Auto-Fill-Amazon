b7f35e9b997c2b5f87409e808a621ef7f0ed307f
MahmoudGlala <beagle663@gmail.com>
2024-10-16 19:39:02 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -11,10 +11,58 @@
     ];
     
     const lastNames = [
-        "Blackwood", "Cunningham", "Davenport", "Everhart", "Fairchild", "Gainsborough", 
-        "Hawthorne", "Ivy", "Jenkins", "Kingsley", "Lancaster", "Montgomery", 
-        "Northwood", "Oakley", "Pemberton", "Quinton", "Ravenwood", "Sinclair", 
-        "Thornfield", "Underwood", "Vanderbilt", "Westbrook", "York", "Zeller"
+    "Blackwood", "Cunningham", "Davenport", "Everhart", "Fairchild", "Gainsborough", 
+    "Hawthorne", "Ivy", "Jenkins", "Kingsley", "Lancaster", "Montgomery", 
+    "Northwood", "Oakley", "Pemberton", "Quinton", "Ravenwood", "Sinclair", 
+    "Thornfield", "Underwood", "Vanderbilt", "Westbrook", "York", "Zeller", 
+    "Ashford", "Brockwell", "Carrington", "Delacroix", "Ellington", "Fitzgerald", 
+    "Greenwood", "Harrington", "Inglewood", "Kensington", "Lockwood", "Merrick", 
+    "Norwood", "Osborne", "Prescott", "Radcliffe", "Sutherland", "Tremont", 
+    "Valentine", "Winslow", "Waverly", "Yardley", "Whitlock", "Wyndham", "Ziegler",
+    "Abernathy", "Bancroft", "Clayton", "Donovan", "Edgerton", "Fleming", 
+    "Garrett", "Huntington", "Irving", "Jacobs", "Kirkland", "Langston", 
+    "Maxwell", "Newbury", "Orwell", "Pembroke", "Quigley", "Roosevelt", 
+    "Somerset", "Trenton", "Upton", "Vaughn", "Whittaker", "Yates", 
+    "Aldridge", "Barclay", "Caldwell", "Dunmore", "Elliot", "Foster", 
+    "Garrison", "Holloway", "Ivers", "Jordan", "Kipling", "Leland", 
+    "Morgan", "Newton", "O'Malley", "Patton", "Quill", "Remington", 
+    "Stanford", "Thatcher", "Ulrich", "Vermont", "Windsor", "Yarborough", 
+    "Anderson", "Bradford", "Carter", "Driscoll", "Ellsworth", "Faulkner", 
+    "Goodwin", "Hudson", "Iverson", "Jefferson", "Keating", "Levinson", 
+    "Morrison", "Norton", "Owens", "Patterson", "Quincy", "Randolph", 
+    "Sheffield", "Torrance", "Urbane", "Vickers", "Wellington", "Youngblood", 
+    "Archer", "Benedict", "Channing", "Dexter", "Emerson", "Fletcher", 
+    "Grant", "Hayes", "Ingram", "Jarvis", "Kendrick", "Lovelace", 
+    "Murray", "Nixon", "Orson", "Pike", "Redwood", "Sterling", 
+    "Thornton", "Urbach", "Vincent", "Wadsworth", "Yancy", "Zimmerman", 
+    "Armstrong", "Bennett", "Colton", "Dalton", "Ellis", "Fleming", 
+    "Grey", "Hamilton", "Ivory", "Justice", "Kane", "Lawrence", 
+    "Monroe", "Nash", "Owensby", "Porter", "Roth", "Stevenson", 
+    "Thorne", "Ullman", "Vargas", "Wallace", "Yarborough", "Zane", 
+    "Baxter", "Carlisle", "Davison", "Easton", "Flanagan", "Gregory", 
+    "Hartley", "Irons", "Jagger", "Kerr", "Langley", "Milton", 
+    "Naylor", "Olsen", "Pierce", "Rothschild", "Sloane", "Templeton", 
+    "Thurlow", "Ulster", "Vernon", "Wainwright", "Yates", "Ziegler", 
+    "Benson", "Campbell", "Dawson", "Evans", "Forbes", "Grayson", 
+    "Harper", "Irving", "Jacobi", "King", "Lloyd", "Marsden", 
+    "Neville", "Orwell", "Pendleton", "Rivers", "Spencer", "Talbot", 
+    "Tyrell", "Underhill", "Voss", "Westerfield", "Yeats", "Zimmer", 
+    "Blake", "Clarke", "Delaney", "Ewing", "Fairbanks", "Gibson", 
+    "Hart", "Iverson", "Johnson", "Keller", "Lewis", "McAllister", 
+    "North", "O'Rourke", "Parrish", "Reynolds", "Stone", "Tanner", 
+    "Thompson", "Upchurch", "Vale", "Weston", "Yorkshire", "Zelman", 
+    "Beaumont", "Chester", "Dixon", "Eddington", "Finley", "Gray", 
+    "Hawkins", "Ives", "Jordan", "Knight", "Lawson", "Marshall", 
+    "Nolan", "Oliver", "Price", "Rogers", "Somers", "Torrington", 
+    "Tully", "Urmston", "Vandermeer", "Whitman", "Young", "Zimmerman", 
+    "Baldwin", "Collins", "Doyle", "Emery", "Ford", "Grant", 
+    "Henderson", "Irving", "Jennings", "Kincaid", "Lowe", "Mitchell", 
+    "Nelson", "Orton", "Palmer", "Richardson", "Stanton", "Travis", 
+    "Thurston", "Ulman", "Vance", "Westlake", "Yarrow", "Zeigler", 
+    "Bryant", "Cook", "Douglas", "Ellison", "Fraser", "Griffith", 
+    "Hughes", "Jameson", "Kent", "Larkin", "Malone", "Norman", 
+    "Parker", "Ramsay", "Sharp", "Taylor", "Thorne", "Upton", 
+    "Vaughn", "Wharton", "Yardley", "Zeller"
     ];
 
     // Set up a counter that increments every time you generate a file
@@ -27,8 +75,8 @@
     // Generate email based on name
     const email = `${randomFirstName.toLowerCase()}${randomLastName.toLowerCase()}@aniuni.shop`;
 
-    // Predefined password
-    const password = "12345@As";
+    // Predefined password add Your passowrd here
+    const password = "Add Your PassWord Here";
 
     // Get the current date and time
     const now = new Date();
